d7d63a5c4599256e0d193f50a90202753481bec6
Richard <riprask@live.com>
2020-04-04 22:54:32 +0100

    Created new project Robofriends

---
+++ src/components/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Jade Skott',
+        username: 'JDizzleRip',
+        email: 'jdskott@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Richard Skott',
+        username: 'Ripraskel',
+        email: 'rpskott@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Matthew Burnett',
+        username: 'Nestar69',
+        email: 'mburnett@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Phil Lucas',
+        username: 'Lucage',
+        email: 'plucas@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Dan Gilbert',
+        username: 'Gilbert487',
+        email: 'dgilbert@gmail.com'
+    }
+];
---
+++ src/containers/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import Cardlist from '../components/Cardlist';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import 'tachyons';
+import Scroll from '../components/Scroll';
+
+class App extends Component {
+    
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchField: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchField: event.target.value});
+    }
+
+    getFilteredList = () => {
+        return (
+            this.state.robots.filter(robot => {
+                return (
+                robot.name.toLocaleLowerCase().includes(
+                    this.state.searchField.toLocaleLowerCase())
+                )
+            })
+        );
+    }
+
+    render() {
+        if (this.state.robots.length === 0){
+            return (
+                <h1>Loading...</h1>
+            )
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f1">Robofriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <Cardlist robots={this.getFilteredList()} /> 
+                    </Scroll>
+                </div>
+            );
+        }
+
+    }
+}
+
+export default App;
